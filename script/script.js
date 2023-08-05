96272e502214bfaf34c8544172e705465b61503b
Arihant Swain <arihantswain06@gmail.com>
2023-08-05 11:14:00 -0600

    fixed null issues

--- script/script.js
+++ script/script.js
@@ -27,25 +27,33 @@ function retry(tryAgain) {
   return false
 }
 
+function avoidNull(userInput) {
+  if (userInput == null) { // edge case--user clicks cancel
+    return true
+  }
+}
+
 function play() {
   let decision = window.prompt("Enter your choice for round " + (round+1) + " of 5: \nrock, paper, or scissors?")
-  let numComputerChoice = computerPlay()
-  let numUserChoice = moves.get(decision.trim().toLowerCase())
   
-  if (numUserChoice == undefined) { // edge case--string entered other than rock, paper, or scissors
-    alert("You entered a non-reliable value, round counted as a tie.")
-    round++;
-    return
+  if (avoidNull(decision)) {
+    let checkQuit = window.prompt("You chose to quit. Do you want to continue?")
+    retry(checkQuit)
+    return "quit"
   }
 
-  if (numUserChoice == null) { // edge case--user clicks cancel
-    let checkQuit = windoww.prompt("It seems you don't want to play against the Ultimate AI... \nAre you sure you want to quit?")
-    if (retry(checkQuit)) {
-      game()
-    } else {
-      alert("Alright, see you around!")
-      return
+  let numComputerChoice = computerPlay()
+  let numUserChoice = moves.get(decision.trim().toLowerCase())
+  console.log(numUserChoice)
+  
+  while (numUserChoice == undefined) { // edge case--string entered other than rock, paper, or scissors
+    console.log("enter")
+    numUserChoice = window.prompt("You entered a non-reliable value, enter a different value")
+    if (avoidNull(numUserChoice)) {
+      return "quit"
     }
+    console.log("here")
+    numUserChoice = moves.get(numUserChoice.trim().toLowerCase())
   }
 
   let outcome = checkWinCondition(numUserChoice, numComputerChoice)
@@ -78,7 +86,10 @@ function checkWinCondition(user, computer) {
 
 function game() {
   for (let i = 0; i < 5; i++) {
-    play()
+    let condition = play()
+    if (condition == "quit") {
+      return
+    }
   }
 
   if (winCounter > lossCounter) {
