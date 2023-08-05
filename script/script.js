95556075992541053291c4e3aeb71d86568c864e
Arihant Swain <arihantswain06@gmail.com>
2023-08-05 10:26:19 -0600

    added comments to script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// map and array to create the equivalent of a bi-dirctional map
 const moves = new Map();
 moves.set("rock", 0)
 moves.set("paper", 1)
@@ -9,19 +10,46 @@ let winCounter = 0
 let lossCounter = 0
 let round = 0
 
+// base alert to start game
+alert("Welcome! \nIn a game of rock, paper, scissors, you choose an option in which: \nscissors beats paper, paper beats rock, rock beats scissors. \n\nThough simple, it can be hard. \n\nGet ready to play against the ultimate AI!")
+
+// calculates computer choice by assigning number 0-2
 function computerPlay() {
   return Math.floor(Math.random() * 3)
 }
 
+// retry function for future prompting
+function retry(tryAgain) {
+  if (tryAgain.trim().toLowerCase() == "yes") {
+    lossCounter = 0
+    winCounter = 0
+    round = 0
+    return true
+  }
+  return false
+}
+
 function play() {
-  let decision = window.prompt("Welcome! \nIn a game of rock, paper, scissors, you choose an option in which: \nscissors beats paper, paper beats rock, rock beats scissors. \n\nThough simple, it can be hard. \n\nReady to play against the ultimate AI? \nEnter your choice for round one of five.")
+  let decision = window.prompt("Enter your choice for round " + (round+1) + " of 5: \nrock, paper, or scissors?")
   let numComputerChoice = computerPlay()
   let numUserChoice = moves.get(decision.trim().toLowerCase())
-  if (numUserChoice == undefined) {
+  
+  if (numUserChoice == undefined) { // edge case--string entered other than rock, paper, or scissors
     alert("You entered a non-reliable value, round counted as a tie.")
     round++;
     return
   }
+
+  if (numUserChoice == null) { // edge case--user clicks cancel
+    let checkQuit = windoww.prompt("It seems you don't want to play against the Ultimate AI... \nAre you sure you want to quit?")
+    if (retry(checkQuit)) {
+      game()
+    } else {
+      alert("Alright, see you around!")
+      return
+    }
+  }
+
   let outcome = checkWinCondition(numUserChoice, numComputerChoice)
   
   round++;
@@ -46,16 +74,6 @@ function checkWinCondition(user, computer) {
       return "lost"
     }
   }
-}
-
-function retry(tryAgain) {
-  if (tryAgain.trim().toLowerCase() == "yes") {
-    lossCounter = 0
-    winCounter = 0
-    round = 0
-    return true
-  }
-  return false
 }
 
 function game() {
@@ -89,6 +107,5 @@ function game() {
     }
   }
 }
-
 
 game()
